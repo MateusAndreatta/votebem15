26a5279c19567e463737dd30b1e0050d61844465
Andreatta <mateus-henrique2001@hotmail.com>
2017-11-15 12:17:51 -0200

    arquivos
    andreatta

---
+++ PaginasProcessamento/inserir_candidatos.php
@@ -0,0 +1,34 @@
+<?php
+	$id                = "";
+    $nome              = $_POST["nome"];
+    $numero            = $_POST["numero"];
+    $cargo             = $_POST["cargo"];
+    $cidade            = $_POST["cidade"];
+    $estado            = $_POST["estado"];
+    $nascimento        = $_POST["nascimento"];
+    $partidos          = $_POST["parti"]; //não reconhece valor do select
+	$Arq               = $_FILES["img"];
+    $nomeArq           = $Arq["name"];
+    $tamanho           = $Arq["size"];
+    $tmp               = $Arq["tmp_name"];
+    $formato           = pathinfo($nomeArq, PATHINFO_EXTENSION);
+    $nomeBdArq         = uniqid().".".$formato;
+	$upload            = move_uploaded_file($tmp, '../imgs/candidatos/'.$nomeBdArq );	
+
+	include "conexao.php";
+	$sql     = "INSERT INTO tb_candidatos VALUES(?,?,?,?,?,?,?,?,?)";
+	$votebem = $banco -> prepare($sql);	
+	$votebem -> execute(array($id,$nome,$nomeBdArq,$numero,$cargo,$cidade,$estado,$nascimento,$partidos));	
+	$votebem = null;
+	
+	echo $id;
+	echo $nome;
+	echo $numero;
+	echo $cargo;
+	echo $cidade;
+	echo $estado;
+	echo $nascimento;
+	echo $partidos;
+	/*header("Location: ../PagsControle/menu_candidatos.php?cadastro=ok");*/
+	
+?>
---
+++ inserir_candidatos.php
@@ -0,0 +1,26 @@
+<?php
+	$id                = "";
+    $nome              = $_POST["nome"];
+    $numero            = $_POST["numero"];
+    $cargo             = $_POST["cargo"];
+    $cidade            = $_POST["cidade"];
+    $estado            = $_POST["estado"];
+    $nascimento        = $_POST["nascimento"];
+    $partidos          = $_POST["partido"]; //não reconhece valor do select
+	$Arq               = $_FILES["img"];
+    $nomeArq           = $Arq["name"];
+    $tamanho           = $Arq["size"];
+    $tmp               = $Arq["tmp_name"];
+    $formato           = pathinfo($nomeArq, PATHINFO_EXTENSION);
+    $nomeBdArq         = uniqid().".".$formato;
+	$upload            = move_uploaded_file($tmp, 'imgs/candidatos/'.$nomeBdArq );	
+
+	include "PaginasProcessamento/conexao.php";
+	$sql     = "INSERT INTO tb_candidatos VALUES(?,?,?,?,?,?,?,?,?)";
+	$votebem = $banco -> prepare($sql);	
+	$votebem -> execute(array($id,$nome,$nomeBdArq,$numero,$cargo,$cidade,$estado,$nascimento,$partidos));	
+	$votebem = null;
+	
+	header("Location: PagsControle/menu_candidatos.php?cadastro=ok");
+	
+?>
