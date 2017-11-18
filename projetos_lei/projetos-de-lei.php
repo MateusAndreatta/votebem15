9465f646836978f605688d5b81ff374247b34e10
Andreatta <mateus-henrique2001@hotmail.com>
2017-11-18 00:42:59 -0200

    arrumei todo o esquema de layout dos cards
    Andreatta

--- projetos_lei/projetos-de-lei.php
+++ projetos_lei/projetos-de-lei.php
@@ -2,169 +2,154 @@
 <html lang="pt-br">
 
 <head>
-	<title>Vote Bem - Projetos de lei</title>
-	<link rel="icon" href="../imgs/favicon.ico">
-	<meta charset="UTF-8">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-	<link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
-	<link rel="stylesheet" href="../css/estilo.css">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Vote Bem - Projetos de lei</title>
+    <link rel="icon" href="../imgs/favicon.ico">
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
+    <link rel="stylesheet" href="../css/estilo.css">
+    <link type="text/css" rel="stylesheet" href="../css/cards.css" media="screen,projection" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 </head>
 
 <body>
-	<main>
-		<?php include "../PaginasProcessamento/nav2.php"; ?>
+    <main>
+        <?php include "../PaginasProcessamento/nav2.php"; ?>
         <nav>
             <div class="nav-wrapper blue espacamento-lateral">
-              <div class="col s12">
-                <a href="../index.php" class="breadcrumb">Home</a>
-                <a href="projetos-de-lei.html" class="breadcrumb">Projetos de lei</a>
-              </div>
+                <div class="col s12">
+                    <a href="../index.php" class="breadcrumb">Home</a>
+                    <a href="projetos-de-lei.html" class="breadcrumb">Projetos de lei</a>
+                </div>
             </div>
-          </nav>
-		<div class="container">
-			<h1 class="center">Projetos de leis</h1>
-			<div class="row">
-				<div class="col s12 m6 l4">
-					<!--inicio do card projeto de lei OLEO DE COZINHA-->
-					<div class="projeto1">
-						<div class="card">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="../imgs/cards/oleo-card.png">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4 flow-text">Reutilização do óleo<i class="material-icons right">more_vert</i></span>
-								<p><a href="projetoReutilizacaoOleo.php">Clique aqui para acessar</a></p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">Reutilização do óleo de cozinha<i class="material-icons right">close</i></span>
-								<p class="justificar">Armazenagem correta do óleo usado em empresas do ramo alimentício , coleta seletiva através da usina de biodiesel móvel ,e parceria com  o município para coleta do óleo utilizado pela população. Descarte da forma correta para reaproveitamento e reutilização do óleo.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m6 l4"> 
-					<!--inicio do card projeto de lei QR CODE-->
-					<div class="projeto1">
-						<div class="card">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="../imgs/cards/qr-code-card.png">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4 flow-text">QR Code<i class="material-icons right">more_vert</i></span>
-								<p><a href="projetoQrcode.php">Clique aqui para acessar</a></p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">Projeto de lei Nº: 409/2016<i class="material-icons right">close</i></span>
-								<p>Dispõe sobre a política de estímulo e incentivo a inserção de qr code nas embalagens de todos os produtos comercializados no estado do paraná.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m6 l4">
-					<!--inicio do card projeto de lei STARTUP-->
-					<div class="projeto1">
-						<div class="card">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="../imgs/cards/startup-card.png">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4 flow-text">Startup<i class="material-icons right">more_vert</i></span>
-								<p><a href="projetoStartup.php">Clique aqui para acessar</a></p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">Projeto de lei Nº: 337/2016<i class="material-icons right">close</i></span>
-								<p>Dispõe sobre a política de estímulo, incentivo e promoção ao desenvolvimento local de startups e indústrias tecnológicas, no estado do paraná e dá outras providências.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m6 l4">
-					<!--inicio do card projeto de lei ENERGIA RENOVAVEL-->
-					<div class="projeto1">
-						<div class="card">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="../imgs/cards/energia.png">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4 flow-text">Energia Renovável<i class="material-icons right">more_vert</i></span>
-								<p><a href="projetoEnergiaRenovavel.php">Clique aqui para acessar</a></p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">Projeto de lei Nº: 942/2015<i class="material-icons right">close</i></span>
-								<p>Dispõe sobre a obrigatoriedade de indústrias situadas no estado do paraná a instalarem equipamentos de energia solar, térmica e/ou eólica.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m6 l4">
-					<!--inicio do card projeto de lei Ipê Amarelo -->
-					<div class="projeto1">
-						<div class="card">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="../imgs/cards/ipe.jpg">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4 flow-text">Plantio de árvore nativa<i class="material-icons right">more_vert</i></span>
-								<p><a href="projetoIpe.php">Clique aqui para acessar</a></p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">Projeto de lei Nº: 333/2016<i class="material-icons right">close</i></span>
-								<p>Estabelece a obrigatoriedade de plantio de uma árvore nativa do paraná nos terrenos que especifica e dá outras providências.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col s12 m6 l4">
-					<!--inicio do card projeto de lei grafico-->
-					<div class="projeto1"> 
-						<div class="card">
-							<div class="card-image waves-effect waves-block waves-light">
-								<img class="activator" src="../imgs/cards/grafico.jpg">
-							</div>
-							<div class="card-content">
-								<span class="card-title activator grey-text text-darken-4 flow-text">ICMS<i class="material-icons right">more_vert</i></span>
-								<p><a href="projetoIcms.php">Clique aqui para acessar</a></p>
-							</div>
-							<div class="card-reveal">
-								<span class="card-title grey-text text-darken-4">Projeto de lei Nº 697/2015<i class="material-icons right">close</i></span>
-								<p>Altera dispositivos da lei nº 15.562, de 7 de julho de 2007, que dispõe sobre o tratamento diferenciado e favorecido a ser dispensado às microempresas e empresas de pequeno porte, no estado do paraná, alterando para r$ 900.000,00 (novecentos mil reais) o parâmetro de isenção do ICMS a ser observado pelas empresas optantes pelo simples nacional.</p>
-							</div>
-						</div>
-					</div>
-				</div>
-				
-			</div>
-		</div>
-		<!-- FIM DOS BLOCOS -->
-	</main>
-	<footer class="page-footer blue">
-		<div class="container">
-			<div class="row">
-				<div class="col l6 s12">
-					<p>
-						<a href="index.php"></a><img src="../imgs/footer-logo.png">
-					</p>
-				</div>
-				<div class="col l4 offset-l2 s12">
-					<h5 class="white-text">Compartilhe</h5>
-					<ul>
-						<li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
-						<li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
-					</ul>
-				</div>
-			</div>
-		</div>
-		<div class="footer-copyright">
-			<div class="container">
-				© 2017 Copyright - Todos os direitos reservados
-			</div>
-		</div>
-	</footer>
-	<script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
-	<script type="text/javascript" src="../js/materialize.min.js"></script>
-	<script type="text/javascript" src="../js/custom.js"></script>
+        </nav>
+        <div class="container">
+            <h1 class="center">Projetos de leis</h1>
+            <div class="row">
+                <div class="cards-container center">
+                    <!--inicio do card projeto de lei OLEO DE COZINHA-->
+
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="../imgs/cards/oleo-card.png">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4 flow-text">Reutilização do óleo<i class="material-icons right">more_vert</i></span>
+                            <p><a href="projetoReutilizacaoOleo.php">Clique aqui para acessar</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Reutilização do óleo de cozinha<i class="material-icons right">close</i></span>
+                            <p class="justificar">Armazenagem correta do óleo usado em empresas do ramo alimentício , coleta seletiva através da usina de biodiesel móvel ,e parceria com o município para coleta do óleo utilizado pela população. Descarte da forma correta para reaproveitamento e reutilização do óleo.</p>
+                        </div>
+                    </div>
+                    <!--inicio do card projeto de lei QR CODE-->
+
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="../imgs/cards/qr-code-card.png">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4 flow-text">QR Code<i class="material-icons right">more_vert</i></span>
+                            <p><a href="projetoQrcode.php">Clique aqui para acessar</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Projeto de lei Nº: 409/2016<i class="material-icons right">close</i></span>
+                            <p>Dispõe sobre a política de estímulo e incentivo a inserção de qr code nas embalagens de todos os produtos comercializados no estado do paraná.</p>
+                        </div>
+                    </div>
+                    <!--inicio do card projeto de lei STARTUP-->
+
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="../imgs/cards/startup-card.png">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4 flow-text">Startup<i class="material-icons right">more_vert</i></span>
+                            <p><a href="projetoStartup.php">Clique aqui para acessar</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Projeto de lei Nº: 337/2016<i class="material-icons right">close</i></span>
+                            <p>Dispõe sobre a política de estímulo, incentivo e promoção ao desenvolvimento local de startups e indústrias tecnológicas, no estado do paraná e dá outras providências.</p>
+                        </div>
+                    </div>
+                    <!--inicio do card projeto de lei ENERGIA RENOVAVEL-->
+
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="../imgs/cards/energia.png">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4 flow-text">Energia Renovável<i class="material-icons right">more_vert</i></span>
+                            <p><a href="projetoEnergiaRenovavel.php">Clique aqui para acessar</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Projeto de lei Nº: 942/2015<i class="material-icons right">close</i></span>
+                            <p>Dispõe sobre a obrigatoriedade de indústrias situadas no estado do paraná a instalarem equipamentos de energia solar, térmica e/ou eólica.</p>
+                        </div>
+                    </div>
+                    <!--inicio do card projeto de lei Ipê Amarelo -->
+
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="../imgs/cards/ipe.jpg">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4 flow-text">Plantio de árvore nativa<i class="material-icons right">more_vert</i></span>
+                            <p><a href="projetoIpe.php">Clique aqui para acessar</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Projeto de lei Nº: 333/2016<i class="material-icons right">close</i></span>
+                            <p>Estabelece a obrigatoriedade de plantio de uma árvore nativa do paraná nos terrenos que especifica e dá outras providências.</p>
+                        </div>
+                    </div>
+                    <!--inicio do card projeto de lei grafico-->
+
+                    <div class="card">
+                        <div class="card-image waves-effect waves-block waves-light">
+                            <img class="activator" src="../imgs/cards/grafico.jpg">
+                        </div>
+                        <div class="card-content">
+                            <span class="card-title activator grey-text text-darken-4 flow-text">ICMS<i class="material-icons right">more_vert</i></span>
+                            <p><a href="projetoIcms.php">Clique aqui para acessar</a></p>
+                        </div>
+                        <div class="card-reveal">
+                            <span class="card-title grey-text text-darken-4">Projeto de lei Nº 697/2015<i class="material-icons right">close</i></span>
+                            <p>Altera dispositivos da lei nº 15.562, de 7 de julho de 2007, que dispõe sobre o tratamento diferenciado e favorecido a ser dispensado às microempresas e empresas de pequeno porte, no estado do paraná, alterando para r$ 900.000,00 (novecentos mil reais) o parâmetro de isenção do ICMS a ser observado pelas empresas optantes pelo simples nacional.</p>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </div>
+        <!-- FIM DOS BLOCOS -->
+    </main>
+    <footer class="page-footer blue">
+        <div class="container">
+            <div class="row">
+                <div class="col l6 s12">
+                    <p>
+                        <a href="index.php"></a><img src="../imgs/footer-logo.png">
+                    </p>
+                </div>
+                <div class="col l4 offset-l2 s12">
+                    <h5 class="white-text">Compartilhe</h5>
+                    <ul>
+                        <li><a class="grey-text text-lighten-3" href="#!">Twitter</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Facebook</a></li>
+                        <li><a class="grey-text text-lighten-3" href="#!">Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                © 2017 Copyright - Todos os direitos reservados
+            </div>
+        </div>
+    </footer>
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript" src="../js/materialize.min.js"></script>
+    <script type="text/javascript" src="../js/custom.js"></script>
 </body>
 
 </html>
