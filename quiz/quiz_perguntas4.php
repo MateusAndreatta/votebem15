cfc73e791ab320ca1ad158ff6be5b88ce7c0a466
VoteBem <32470677+VoteBem@users.noreply.github.com>
2017-11-22 16:33:02 -0200

    erros corrigidos - 22/11

--- quiz/quiz_perguntas4.php
+++ quiz/quiz_perguntas4.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!DOCTYPE html>
 <html lang="pt-br">
 	<head>
@@ -130,62 +129,3 @@
 		<script type="text/javascript" src="../js/custom.js"></script>
 	</body>
 </html>
-=======
-<!DOCTYPE html>
-<html lang="pt-br">
-	<head>
-		<title>Vote Bem - Quiz</title>
-		<meta charset="UTF-8">
-		<link rel="icon" href="../imgs/favicon.ico">
-		<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-		<link type="text/css" rel="stylesheet" href="../css/materialize.min.css" media="screen,projection" />
-		<link rel="stylesheet" href="../css/estilo.css">
-		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
-	</head>
-	<?php
-		$questao1 = $_POST['questao1'];
-		$questao2 = $_POST['questao2'];
-		$questao3 = $_POST['group1'];
-		
-		session_start();
-		if(isset($_POST['action'])){
-			$_SESSION['questao'] = 4;
-		}
-
-		if(isset($_SESSION['questao'])){
-		if($_SESSION['questao'] == 4){
-		}else{
-			header("Location: quiz_perguntas.php");
-		}
-	}else{
-		header("Location: quiz_perguntas.php");
-	}
-
-		$contcerto = 0;
-		$conterrado = 0;
-		
-		if($questao1 == 2){
-			$contcerto++;
-		}else{
-			$conterrado++;
-		}
-		if($questao2 == 1){
-			$contcerto++;
-		}else{
-			$conterrado++;
-		}
-		if($questao3 == 1){
-			$contcerto++;
-		}else{
-			$conterrado++;
-		}
-		
-		echo "<script>
-			alert('Você acertou $contcerto/3');
-			window.location.href = '../index.php'
-		</script>";
-		
-		
-	?>
->>>>>>> c2f3d153dccfb621f8afd0daf6a7d12c4624571e
- 
